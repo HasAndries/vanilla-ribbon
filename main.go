b40b445c0c951f38be798cc390657458b42b4e26
Andries Coetzee <andriesc@lime-square.net>
2014-11-18 13:03:09 +0200

    testing

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"path"
+	"text/template"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	var pageName string
+	if pageName = r.URL.Path[1:]; pageName == "" {
+		pageName = "index"
+	}
+	lp := path.Join("views", "layout.html")
+	fp := path.Join("views", pageName+".html")
+
+	// Note that the layout file must be the first parameter in ParseFiles
+	tmpl, err := template.ParseFiles(lp, fp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, profile); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func main() {
