f0b7550c368fcfbb74546f6161b4e8bebecde9a0
Andries Coetzee <andriesc@lime-square.net>
2014-11-18 14:53:41 +0200

    added some handlers

--- main.go
+++ main.go
@@ -2,26 +2,30 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"path"
 	"text/template"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	var pageName string
-	if pageName = r.URL.Path[1:]; pageName == "" {
-		pageName = "index"
+	params := make(map[string]string)
+	if params["page"] = r.URL.Path[1:]; params["page"] == "" {
+		params["page"] = "index"
 	}
-	lp := path.Join("views", "layout.html")
-	fp := path.Join("views", pageName+".html")
+	layoutPath := path.Join("views", "layout.html")
+	contentPath := path.Join("views", params["page"]+".html")
 
-	// Note that the layout file must be the first parameter in ParseFiles
-	tmpl, err := template.ParseFiles(lp, fp)
+	if _, err := os.Stat(contentPath); err != nil {
+		contentPath = path.Join("views", "not-found.html")
+	}
+
+	tmpl, err := template.ParseFiles(layoutPath, contentPath)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	if err := tmpl.Execute(w, profile); err != nil {
+	if err := tmpl.Execute(w, params); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
